ec92eca450a77875675d5d56d7da54899576d631
Vincent Li <vinnyli@Vincents-MacBook-Pro.local>
2023-10-03 14:36:05 -0400

    Project 2 Solution

--- Project1Li.java
+++ Project1Li.java
@@ -9,14 +9,14 @@ public class Project1Li
     {
         Scanner keyboard = new Scanner(System.in);
         String processorType, overclock, programTitle, resolution, recommendedGraphics, overclockWarning;
-        double gpuClockSpeed, cpuClockSpeed, performanceScore;
+        double gpuClockSpeed, cpuClockSpeed, performanceScore, multiplier = 0;
         int processorCores;
         char answer;
         
         System.out.print("Please enter the type of processor: ");
         processorType = keyboard.nextLine();
 
-        System.out.print("Please enter the clock speed(Megahertz) of your graphics card: ");
+        System.out.print("Please enter the clock speed(Megahertz) of youx`r graphics card: ");
         gpuClockSpeed = keyboard.nextDouble();
 
         System.out.print("Please enter the clock speed(Megahertz) of your processor: ");
@@ -31,8 +31,29 @@ public class Project1Li
         overclock = keyboard.nextLine();
         answer = overclock.charAt(0);
 
-        System.out.print(" 1. 1280x720 \n 2. 1920x1080 \n 3. 2560x1440 \n 4. 3840x2160. \n");
+        System.out.print(" 1. 1280x720 \n 2. 1920x1080 \n 3. 2560x1440 \n 4. 3840x2160 \n");
         resolution = keyboard.nextLine();
+
+        switch (resolution)
+        {
+            case "1":
+                multiplier = 1;
+                break;
+            case "2":
+                multiplier = 0.75;
+                break;
+            case "3":
+                multiplier = 0.55;
+                break;
+            case "4":
+                multiplier = 0.35;
+                break;
+            default:
+                System.out.println("That is not a valid resolution option.");
+
+        }
+
+        performanceScore = ((5 * gpuClockSpeed) + (4 * processorCores)) * multiplier;
 
         System.out.println("Computer Hardware Recommendation Tool.");
         System.out.println("Processor Type: " + processorType);
