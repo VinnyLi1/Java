c90a2618b60aa89f401d5f088d2a50025c39f9f4
Vincent Li <vinnyli@Vincents-MacBook-Pro.local>
2023-11-02 12:49:00 -0400

    Chapter 4 practice 2 Updated with writing to file

--- Chapter4Prac2.java
+++ Chapter4Prac2.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
+import java.io.*;
 
 public class Chapter4Prac2 
 {
-    public static void main(String[]args)
+    public static void main(String[]args) throws IOException
     {
         Scanner keyboard = new Scanner(System.in);
+        PrintWriter outputFile = new PrintWriter("DistanceTraveled.txt");
+
         int distance, speed, time;
 
         System.out.println("How fast is the train going(Miles)? ");
@@ -13,14 +16,15 @@ public class Chapter4Prac2
         System.out.println("How long is the train moving(Hours)? ");
         time = keyboard.nextInt();
 
-        System.out.println("Hour" + "\tDistance Traveled");
+        outputFile.println("Hour" + "\tDistance Traveled");
 
         for (int constant = 1; constant <= time; constant++)
         {
             distance = constant * speed;
-            System.out.println(constant + "\t" + distance);
+            outputFile.println(constant + "\t\t\t" + distance);
         }
 
         keyboard.close();
+        outputFile.close();
     }
 }
---
+++ Chapter4Prac3.java
@@ -0,0 +1,7 @@
+public class Chapter4Prac3 
+{
+    public static void main(String[]args)
+    {
+        
+    }
+}
